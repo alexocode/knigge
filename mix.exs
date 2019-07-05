dc27d2d8134e27cb5e3f4437d2d5655c07ec1290
Sascha Wolf <swolf.dev@gmail.com>
2019-07-05 13:31:37 +0200

    Deps: Add credo

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule Knigge.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
   end
 end
