5d64ac71f966c9184838b7b4faea1e734858f58a
Sascha Wolf <swolf.dev@gmail.com>
2019-07-05 15:50:00 +0200

    Knigge: Improve the tests

--- test/knigge_test.exs
+++ test/knigge_test.exs
@@ -3,7 +3,7 @@ defmodule KniggeTest do
 
   import Mox
 
-  describe "SomeBehaviour using Knigge" do
+  describe "a behaviour using Knigge with the direct :implementation option" do
     defmodule SomeBehaviour do
       use Knigge, implementation: SomeBehaviourMock
 
@@ -31,6 +31,45 @@ defmodule KniggeTest do
       assert_receive {:argument, [1, 2, 3, 4]}
 
       verify!(SomeBehaviourMock)
+    end
+
+    test "defines a __knigge__(:implementation) function" do
+      assert SomeBehaviour.__knigge__(:implementation) == SomeBehaviourMock
+    end
+  end
+
+  describe "a behaviour using Knigge with the :otp_app key" do
+    test "raises an ArgumentError without a relevant configuration available" do
+      assert_raise ArgumentError,
+                   ~r/could not fetch application environment KniggeTest.RaisingBehaviour for application :missing/,
+                   fn ->
+                     defmodule RaisingBehaviour do
+                       use Knigge, otp_app: :missing
+                     end
+                   end
+    end
+
+    test "works fine with the relevant configuration being set in the Application environment" do
+      Application.put_env(:knigge, __MODULE__.WorkingBehaviour, SomeModule)
+
+      # Should not raise
+      defmodule WorkingBehaviour do
+        use Knigge, otp_app: :knigge
+      end
+
+      assert WorkingBehaviour.__knigge__(:implementation) == SomeModule
+    end
+  end
+
+  describe "a behaviour using Knigge without the :implementation and no :otp_app key" do
+    test "raises an ArgumentError with a descriptive error" do
+      assert_raise ArgumentError,
+                   "Knigge expects either an :implementation or :otp_app key but neither was given.",
+                   fn ->
+                     defmodule AnotherBehaviour do
+                       use Knigge
+                     end
+                   end
     end
   end
 end
