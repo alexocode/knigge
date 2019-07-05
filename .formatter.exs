24acb2ffa47883bdf92e33a651ccd058baeba7e2
Sascha Wolf <swolf.dev@gmail.com>
2019-07-05 14:56:34 +0200

    Knigge: Add tests for the basic delegation functionality

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    defmock: 2,
+    send: 2
+  ]
 ]
--- test/knigge_test.exs
+++ test/knigge_test.exs
@@ -1,8 +1,36 @@
 defmodule KniggeTest do
-  use ExUnit.Case
-  doctest Knigge
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert Knigge.hello() == :world
+  import Mox
+
+  describe "SomeBehaviour using Knigge" do
+    defmodule SomeBehaviour do
+      use Knigge, implementation: SomeBehaviourMock
+
+      @callback my_function(argument :: any()) :: any()
+      @callback another_function(list()) :: list()
+    end
+
+    defmock SomeBehaviourMock, for: SomeBehaviour
+
+    test "calling my_function/1 delegates the call to SomeBehaviourMock" do
+      expect(SomeBehaviourMock, :my_function, fn arg -> send self(), {:argument, arg} end)
+
+      SomeBehaviour.my_function("with some argument")
+
+      assert_receive {:argument, "with some argument"}
+
+      verify!(SomeBehaviourMock)
+    end
+
+    test "calling another_function/1 delegates the call to SomeBehaviourMock" do
+      expect(SomeBehaviourMock, :another_function, fn arg -> send self(), {:argument, arg} end)
+
+      SomeBehaviour.another_function([1, 2, 3, 4])
+
+      assert_receive {:argument, [1, 2, 3, 4]}
+
+      verify!(SomeBehaviourMock)
+    end
   end
 end
