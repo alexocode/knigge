41a264cdda532e7ddf491e1e5d6b3d927b7f3d0d
Sascha Wolf <swolf.dev@gmail.com>
2019-07-12 16:36:10 +0200

    Knigge: Enable the user to define default implementations for optional callbacks

--- lib/knigge/delegation.ex
+++ lib/knigge/delegation.ex
@@ -1,25 +1,73 @@
 defmodule Knigge.Delegation do
   alias Knigge.Warnings, as: Warn
 
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__), only: [defdefault: 2]
+
+      @before_compile unquote(__MODULE__)
+
+      Module.register_attribute(__MODULE__, :__knigge_fallbacks__, accumulate: true)
+    end
+  end
+
+  defmacro defdefault({name, _meta, args}, do: block) do
+    args = args || []
+    key = {name, length(args)}
+    value = {Macro.escape(args), Macro.escape(block)}
+
+    quote do
+      @__knigge_fallbacks__ {unquote(key), unquote(value)}
+    end
+  end
+
   defmacro __before_compile__(%{module: module}) do
     generate(module)
   end
 
   def generate(module) do
+    callbacks = get_callbacks(module)
+    _optional_callbacks = get_optional_callbacks(module)
     do_not_delegate = get_do_not_delegate_option(module)
     definitions = get_definitions(module)
     implementation = get_implementation(module)
+    fallbacks = get_fallbacks(module)
 
-    module
-    |> Knigge.Behaviour.callbacks()
-    |> Enum.reject(&(&1 in do_not_delegate))
-    |> Enum.map(fn callback ->
-      if callback in definitions do
-        Warn.definition_matching_callback(module, callback)
-      else
-        callback_to_defdelegate(callback, from: module, to: implementation)
+    for callback <- callbacks do
+      cond do
+        callback in do_not_delegate ->
+          :ok
+
+        callback in definitions ->
+          Warn.definition_matching_callback(module, callback)
+
+        Map.has_key?(fallbacks, callback) ->
+          # unless callback in optional_callbacks do
+          #   raise ArgumentError
+          # end
+
+          fallback = Map.get(fallbacks, callback)
+
+          callback_to_defdefault(callback, to: implementation, fallback: fallback)
+
+        true ->
+          callback_to_defdelegate(callback, to: implementation)
       end
-    end)
+    end
+  end
+
+  defp get_callbacks(module) do
+    Knigge.Behaviour.callbacks(module)
+  end
+
+  defp get_optional_callbacks(module) do
+    Knigge.Behaviour.optional_callbacks(module)
+  end
+
+  defp get_do_not_delegate_option(module) do
+    module
+    |> Knigge.fetch!(:options)
+    |> Keyword.get(:do_not_delegate, [])
   end
 
   defp get_implementation(module) do
@@ -30,21 +78,33 @@ defmodule Knigge.Delegation do
     Module.definitions_in(module)
   end
 
-  defp get_do_not_delegate_option(module) do
+  defp get_fallbacks(module) do
     module
-    |> Knigge.fetch!(:options)
-    |> Keyword.get(:do_not_delegate, [])
+    |> Module.get_attribute(:__knigge_fallbacks__)
+    |> Map.new()
   end
 
-  def callback_to_defdelegate({name, arity}, from: module, to: implementation) do
-    args = Macro.generate_arguments(arity, module)
-
+  defp callback_to_defdefault(
+         {name, arity},
+         to: implementation,
+         fallback: {args, block}
+       ) do
     quote do
-      defdelegate unquote(name)(unquote_splicing(args)), to: unquote(implementation)
+      def unquote(name)(unquote_splicing(args)) do
+        if function_exported?(unquote(implementation), unquote(name), unquote(arity)) do
+          apply(unquote(implementation), unquote(name), unquote(args))
+        else
+          unquote(block)
+        end
+      end
     end
   end
 
-  def callback_to_defdelegate(callback) do
-    raise ArgumentError, "do not know how to handle callback: #{inspect(callback)}"
+  defp callback_to_defdelegate({name, arity}, to: implementation) do
+    quote bind_quoted: [name: name, arity: arity, implementation: implementation] do
+      args = Macro.generate_arguments(arity, __MODULE__)
+
+      defdelegate unquote(name)(unquote_splicing(args)), to: implementation
+    end
   end
 end
---
+++ test/behaviour/with_optional_callbacks_test.exs
@@ -0,0 +1,110 @@
+defmodule Behaviour.WithOptionalCallbacksTest do
+  use ExUnit.Case, async: true
+
+  import Knigge.Test.SaltedModule
+
+  require Mox
+
+  defmacrop define_facade(do: block) do
+    quote do
+      implementation = salt_atom(Implementation)
+
+      behaviour =
+        defmodule_salted Behaviour do
+          use Knigge, implementation: implementation
+
+          @callback my_required_function() :: no_return
+          @callback my_optional_function() :: no_return
+          @callback my_optional_function_with_arguments(any(), any()) :: no_return
+
+          @optional_callbacks [my_optional_function: 0, my_optional_function_with_arguments: 2]
+
+          unquote(block)
+        end
+
+      %{facade: behaviour, behaviour: behaviour, implementation: implementation}
+    end
+  end
+
+  defp define_facade, do: define_facade(do: :ok)
+
+  test "does generate delegation for optional callbacks" do
+    %{behaviour: behaviour, facade: facade, implementation: implementation} = define_facade()
+
+    Mox.defmock(implementation, for: behaviour)
+    Mox.expect(implementation, :my_optional_function, fn -> :ok end)
+
+    facade.my_optional_function()
+
+    Mox.verify!(implementation)
+  end
+
+  test "does raise an UndefinedFunctionError when the implementation does not implement the optional callback" do
+    %{behaviour: behaviour, facade: facade, implementation: implementation} = define_facade()
+
+    Mox.defmock(implementation, for: behaviour, skip_optional_callbacks: [my_optional_function: 0])
+
+    assert_raise UndefinedFunctionError,
+                 "function #{inspect(implementation)}.my_optional_function/0 is undefined or private",
+                 fn -> facade.my_optional_function() end
+  end
+
+  test "invokes the default when the implementation does not implement the optional callback" do
+    %{facade: facade} =
+      define_facade do
+        defdefault my_optional_function do
+          send self(), {__MODULE__, :fallback_invoked, []}
+
+          :my_great_fallback
+        end
+
+        defdefault my_optional_function_with_arguments(arg1, arg2) do
+          send self(), {__MODULE__, :fallback_with_arguments_invoked, [arg1, arg2]}
+
+          :my_great_fallback_with_arguments
+        end
+      end
+
+    assert :my_great_fallback == facade.my_optional_function()
+    assert_receive {^facade, :fallback_invoked, []}
+
+    assert :my_great_fallback_with_arguments ==
+             facade.my_optional_function_with_arguments(42, 1337)
+
+    assert_receive {^facade, :fallback_with_arguments_invoked, [42, 1337]}
+  end
+
+  test "does not invoke the default when the implementation actually implements the optional callback" do
+    %{behaviour: behaviour, facade: facade, implementation: implementation} =
+      define_facade do
+        defdefault my_optional_function do
+          send self(), {__MODULE__, :fallback_invoked, []}
+
+          :my_great_fallback
+        end
+
+        defdefault my_optional_function_with_arguments(arg1, arg2) do
+          send self(), {__MODULE__, :fallback_with_arguments_invoked, [arg1, arg2]}
+
+          :my_great_fallback_with_arguments
+        end
+      end
+
+    Mox.defmock(implementation,
+      for: behaviour,
+      skip_optional_callbacks: [my_optional_function_with_arguments: 2]
+    )
+
+    Mox.expect(implementation, :my_optional_function, fn -> :my_great_implementation end)
+
+    assert :my_great_implementation == facade.my_optional_function()
+    refute_receive {^facade, :fallback_invoked, []}
+
+    Mox.verify!(implementation)
+
+    assert :my_great_fallback_with_arguments ==
+             facade.my_optional_function_with_arguments(42, 1337)
+
+    assert_receive {^facade, :fallback_with_arguments_invoked, [42, 1337]}
+  end
+end
