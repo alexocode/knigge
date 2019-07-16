bb1e5c957a37bd4cbf506ec813dc76c7f04254e0
Sascha Wolf <swolf.dev@gmail.com>
2019-07-16 13:30:36 +0200

    Knigge: Set the version to 1.0.0
    Also configure for hex release

--- mix.exs
+++ mix.exs
@@ -1,15 +1,26 @@
 defmodule Knigge.MixProject do
   use Mix.Project
 
+  @version "version" |> File.read!() |> String.trim()
+
   def project do
     [
       app: :knigge,
       version: "0.1.0",
-      # TODO: Check lowest possible version
-      elixir: "~> 1.9",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Knigge",
+      source_url: "https://github.com/sascha-wolf/knigge",
+      homepage_url: "https://github.com/sascha-wolf/knigge",
+
+      # Hex
+      description: description(),
+      package: package(),
+      version: @version
     ]
   end
 
@@ -26,9 +37,40 @@ defmodule Knigge.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # No Runtime
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:ex_doc, version(:ex_doc), only: :dev, runtime: false},
+
+      # Test
       {:mox, "~> 0.5", only: :test}
+    ]
+  end
+
+  defp version(:ex_doc) do
+    if Version.match?(System.version(), "< 1.7.0") do
+      "~> 0.18.0"
+    else
+      "~> 0.19"
+    end
+  end
+
+  #######
+  # Hex #
+  #######
+
+  def description do
+    "An opinionated set of rules to for your behaviours"
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "LICENSE*", "README*", "version"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/sascha-wolf/knigge"
+      },
+      maintainers: ["Sascha Wolf <swolf.dev@gmail.com>"]
     ]
   end
 end
