43fcabcc276565d72c1ae915a1a55e809806096b
Sascha Wolf <swolf.dev@gmail.com>
2019-07-26 10:33:33 +0200

    Formatter: Export `defdefault/2` as `locals_without_parens`

--- .formatter.exs
+++ .formatter.exs
@@ -1,8 +1,8 @@
 # Used by "mix format"
+locals_without_parens = [defdefault: 2]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    defmock: 2,
-    send: 2
-  ]
+  locals_without_parens: locals_without_parens ++ [defmock: 2, send: 2],
+  export: [locals_without_parens: locals_without_parens]
 ]
