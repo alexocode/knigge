a90a32ebfe7178bfcb2ebaf0967c76e66195a964
Sascha Wolf <swolf.dev@gmail.com>
2020-02-28 15:23:35 +0100

    Mix.Task: Have a first working version for `knigge.verify`

--- lib/knigge/cli/output.ex
+++ lib/knigge/cli/output.ex
@@ -1,14 +1,8 @@
 defmodule Knigge.CLI.Output do
   @moduledoc false
 
-  def linebreak, do: IO.puts("")
-
-  def info(message, meta), do: Bunt.puts([:blue, format(message, meta)])
-  def success(message, meta), do: Bunt.puts([:green, format(message, meta)])
-  def error(message, meta), do: Bunt.warn([:red, format(message, meta)])
-  def warn(message, meta), do: Bunt.warn([:gold, format(message, meta)])
-
-  def format(message, app: app) do
-    ["[#{app}] ", message]
-  end
+  def info(message), do: Bunt.puts([:blue, message])
+  def success(message), do: Bunt.puts([:green, message])
+  def error(message), do: Bunt.warn([:red, message])
+  def warn(message), do: Bunt.warn([:gold, message])
 end
--- lib/mix/tasks/knigge/verify.ex
+++ lib/mix/tasks/knigge/verify.ex
@@ -3,6 +3,17 @@ defmodule Mix.Tasks.Knigge.Verify do
 
   import Knigge.CLI.Output
 
+  defmodule Context do
+    @moduledoc false
+
+    defstruct app: nil,
+              modules: [],
+              existing: [],
+              missing: [],
+              error: nil,
+              began_at: nil
+  end
+
   @recursive true
 
   @exit_codes %{
@@ -14,93 +25,148 @@ defmodule Mix.Tasks.Knigge.Verify do
 
   @impl Mix.Task
   def run(_args) do
-    began_at = current_millis()
-    app = calling_app()
+    calling_app()
+    |> run_for()
+    |> exit_with()
+  end
 
-    app
-    |> begin()
-    |> fetch_modules_to_check()
-    |> verify_implementations(app)
-    |> finish(app, began_at)
-    |> exit_with(app)
+  defp calling_app do
+    Mix.Task.run("compile")
+
+    Mix.Project.get().project()[:app]
+  end
+
+  defp run_for(app) do
+    began_at = current_millis()
+
+    with {:ok, modules} <- fetch_modules_to_check(app) do
+      %Context{
+        app: calling_app(),
+        modules: modules,
+        began_at: began_at
+      }
+      |> begin()
+      |> verify_implementations()
+      |> finish()
+    end
   end
 
   defp current_millis, do: :os.system_time(:millisecond)
 
-  defp calling_app, do: Mix.Project.get().project()[:app]
-
-  defp begin(app) do
-    info("Verify Knigge facades.", app: app)
-
-    app
-  end
-
   defp fetch_modules_to_check(app) do
-    Mix.Task.run("compile")
-
     with :ok <- ensure_loaded(app),
-         {:error, :undefined} <- Knigge.Module.fetch_for_app(app) do
-      error("Unable to load modules for #{app}, are you sure the app exists?", app: app)
+         {:ok, modules} <- Knigge.Module.fetch_for_app(app) do
+      {:ok, modules}
+    else
+      {:error, :undefined} ->
+        error("Unable to load modules for #{app || "current app"}, are you sure the app exists?")
 
-      {:error, :unknown_app}
+        {:error, :unknown_app}
+
+      other ->
+        other
     end
   end
+
+  defp ensure_loaded(nil), do: {:error, :undefined}
 
   defp ensure_loaded(app) do
     case Application.load(app) do
       :ok -> :ok
       {:error, {:already_loaded, ^app}} -> :ok
+      {:error, {'no such file or directory', _}} -> {:error, :undefined}
       other -> other
     end
   end
 
-  defp verify_implementations({:ok, []}, app) do
-    warn("No modules in `#{app}` found which `use Knigge`.", app: app)
+  defp begin(%Context{app: app, modules: modules} = context) do
+    info("Verify #{length(modules)} Knigge facades in '#{app}'.")
 
-    :ok
+    context
   end
 
-  defp verify_implementations({:ok, modules}, app) do
-    Enum.reduce(modules, :ok, fn module, result ->
-      module
-      |> verify_implementation(app)
-      |> to_result(result)
-    end)
+  defp verify_implementations(%Context{app: app, modules: []} = context) do
+    warn("\nNo modules in `#{app}` found which `use Knigge`.")
+
+    context
   end
 
-  defp verify_implementations(other, _app), do: other
+  defp verify_implementations(context) do
+    context.modules
+    |> Enum.map(&verify_implementation/1)
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+    |> merge_with_context(context)
+  end
 
-  defp verify_implementation(module, app) do
-    implementation = module.__knigge__(:implementation)
-
-    if Knigge.Module.exists?(implementation) do
-      success("#{inspect(module)} -> #{inspect(implementation)} (exists)", app: app)
-    else
-      error("#{inspect(module)} -> #{inspect(implementation)} (missing)", app: app)
+  defp verify_implementation(module) do
+    case Knigge.Verify.implementation(module) do
+      {:ok, implementation} -> {:existing, {module, implementation}}
+      {:error, {:missing, implementation}} -> {:missing, {module, implementation}}
     end
   end
 
-  defp to_result(:ok, result), do: result
-  defp to_result({:error, _} = error, _result), do: error
-
-  defp finish(result, app, began_at) do
-    duration = Float.round((current_millis() - began_at) / 1_000, 3)
-
-    info("Completed in #{duration} seconds.", app: app)
-    linebreak()
-
-    result
+  defp merge_with_context(%{missing: _} = result, context) do
+    context
+    |> Map.put(:error, :missing_module)
+    |> Map.merge(result)
   end
 
-  defp exit_with({:error, reason}, _app) when reason in @exit_reasons do
+  defp merge_with_context(result, context), do: Map.merge(context, result)
+
+  defp finish(context) do
+    print_result(context)
+    completed_in(context)
+
+    maybe_error(context)
+  end
+
+  defp print_result(context) do
+    print_existing(context)
+    print_missing(context)
+  end
+
+  defp print_existing(%Context{existing: []}), do: :ok
+
+  defp print_existing(%Context{existing: facades, modules: modules}) do
+    success("\n#{length(facades)}/#{length(modules)} Facades passed:")
+
+    facades
+    |> Enum.map_join("\n", fn {module, implementation} ->
+      "  #{inspect(module)} -> #{inspect(implementation)}"
+    end)
+    |> success()
+  end
+
+  defp print_missing(%Context{missing: []}), do: :ok
+
+  defp print_missing(%Context{missing: facades, modules: modules}) do
+    error("\n#{length(facades)}/#{length(modules)} Facades failed:")
+
+    facades
+    |> Enum.map_join("\n", fn {module, implementation} ->
+      "  #{inspect(module)} -> #{inspect(implementation)}"
+    end)
+    |> error()
+  end
+
+  defp completed_in(%Context{began_at: began_at}) do
+    duration = Float.round((current_millis() - began_at) / 1_000, 3)
+
+    info("\nCompleted in #{duration} seconds.\n")
+  end
+
+  defp maybe_error(%Context{error: nil}), do: :ok
+  defp maybe_error(%Context{error: reason}), do: {:error, reason}
+
+  defp exit_with({:error, reason}) when reason in @exit_reasons do
     exit({:shutdown, @exit_codes[reason]})
   end
 
-  defp exit_with({:error, unknown_reason}, app) do
-    error("An unknown error occurred: #{inspect(unknown_reason)}", app: app)
+  defp exit_with({:error, unknown_reason}) do
+    error("An unknown error occurred: #{inspect(unknown_reason)}")
 
     exit({:shutdown, @unknown_error_code})
   end
 
-  defp exit_with(_, _app), do: :ok
+  defp exit_with(_), do: :ok
 end
