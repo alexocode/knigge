55b66b9fa063849f4ce5ec050fd04b69d1377da3
Sascha Wolf <swolf.dev@gmail.com>
2020-02-28 11:07:33 +0100

    Module: Rewrite the tests as doctests

--- lib/knigge/module.ex
+++ lib/knigge/module.ex
@@ -3,6 +3,8 @@ defmodule Knigge.Module do
 
   alias Knigge.Options
 
+  module = inspect(__MODULE__)
+
   def ensure_exists!(module, opts, env) do
     unless Knigge.Module.exists?(module, opts) do
       Knigge.Error.module_not_loaded!(module, env)
@@ -11,6 +13,29 @@ defmodule Knigge.Module do
     module
   end
 
+  @doc """
+  Returns true if a module exists, false otherwise. Always returns true if
+  `check_if_exists?` is set to `false` on the `Knigge.Options` struct.
+
+  ## Examples
+
+    iex> options = %Knigge.Options{check_if_exists?: true}
+    iex> #{module}.exists?(This.Does.Not.Exist, options)
+    false
+
+    iex> options = %Knigge.Options{check_if_exists?: false}
+    iex> #{module}.exists?(This.Does.Not.Exist, options)
+    true
+
+    iex> options = %Knigge.Options{check_if_exists?: true}
+    iex> #{module}.exists?(Knigge, options)
+    true
+
+    iex> options = %Knigge.Options{check_if_exists?: false}
+    iex> #{module}.exists?(Knigge, options)
+    true
+  """
+  @spec exists?(module :: module()) :: boolean()
   def exists?(module, %Options{} = opts) do
     if opts.check_if_exists? do
       exists?(module)
@@ -19,6 +44,18 @@ defmodule Knigge.Module do
     end
   end
 
+  @doc """
+  Returns true if a module exists, false otherwise.
+
+  ## Examples
+
+      iex> #{module}.exists?(This.Does.Not.Exist)
+      false
+
+      iex> #{module}.exists?(Knigge)
+      true
+  """
+  @spec exists?(module :: module()) :: boolean()
   def exists?(module) do
     Module.open?(module) or Code.ensure_loaded?(module)
   end
--- test/knigge/module_test.exs
+++ test/knigge/module_test.exs
@@ -1,15 +1,5 @@
 defmodule Knigge.ModuleTest do
   use ExUnit.Case, async: true
 
-  alias Knigge.Module
-
-  describe ".exists?/1" do
-    test "returns false for a non-existing module" do
-      assert Module.exists?(This.Does.Not.Exist) == false
-    end
-
-    test "returns true for an existing module" do
-      assert Module.exists?(Knigge) == true
-    end
-  end
+  doctest Knigge.Module
 end
