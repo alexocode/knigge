ef441ba9ddfd230dda27ed7b9126d2096f6733a1
Sascha Wolf <swolf.dev@gmail.com>
2020-02-28 12:08:02 +0100

    Deps: Add `bunt` as a dependency

--- mix.exs
+++ mix.exs
@@ -44,6 +44,8 @@ defmodule Knigge.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:bunt, "~> 0.2"},
+
       # No Runtime
       {:credo, ">= 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
