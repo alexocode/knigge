379dd1f54babbfb7d8ec5ad8aa6c672b45c0a427
Sascha Wolf <swolf.dev@gmail.com>
2020-03-06 10:11:39 +0100

    CLI - Output: By default print to stdio and allow passing stderr

--- lib/knigge/cli/output.ex
+++ lib/knigge/cli/output.ex
@@ -1,8 +1,15 @@
 defmodule Knigge.CLI.Output do
   @moduledoc false
 
-  def info(message), do: Bunt.puts([:blue, message])
-  def success(message), do: Bunt.puts([:green, message])
-  def error(message), do: Bunt.warn([:red, message])
-  def warn(message), do: Bunt.warn([:gold, message])
+  @device :stdio
+
+  def info(device \\ @device, message), do: print(device, [:blue, message])
+  def success(device \\ @device, message), do: print(device, [:green, message])
+  def error(device \\ @device, message), do: print(device, [:red, message])
+  def warn(device \\ @device, message), do: print(device, [:gold, message])
+
+  def print(device \\ @device, message)
+
+  def print(:stdio, message), do: Bunt.puts(message)
+  def print(:stderr, message), do: Bunt.warn(message)
 end
