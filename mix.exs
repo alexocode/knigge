36056d5ad309a346aee25027a69229a420e90e4c
Sascha Wolf <swolf.dev@gmail.com>
2021-03-26 10:11:35 +0100

    Deps: Update dialyzer

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Knigge.MixProject do
 
       # No Runtime
       {:credo, ">= 1.0.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
 
       # Test
