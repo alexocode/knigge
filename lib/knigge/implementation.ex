dfdda737242d24d028643ebe8a2523a3c5928fa3
Marcus Kruse <esurk.sucram@gmail.com>
2021-03-30 07:03:54 +0200

    Update option transformation

--- lib/knigge/implementation.ex
+++ lib/knigge/implementation.ex
@@ -9,11 +9,13 @@ defmodule Knigge.Implementation do
 
   alias Knigge.Options
 
-  def fetch!(%Options{implementation: {:config, otp_app, key}, default: default}) do
-    if is_nil(default) do
-      Application.fetch_env!(otp_app, key)
-    else
-      Application.get_env(otp_app, key, default)
+  def fetch!(%Options{implementation: {:config, otp_app, [key | keys]}, default: default}) do
+    with nil <- otp_app |> env!(key, default) |> get(keys) do
+      raise ArgumentError,
+        message: """
+        could not fetch application environment #{inspect([key | keys])} \
+        for application #{inspect(otp_app)}\
+        """
     end
   end
 
@@ -26,4 +28,14 @@ defmodule Knigge.Implementation do
     |> Knigge.options!()
     |> Knigge.Implementation.fetch!()
   end
+
+  defp env!(otp_app, key, nil), do: Application.fetch_env!(otp_app, key)
+
+  defp env!(otp_app, key, default), do: Application.get_env(otp_app, key, default)
+
+  defp get(nil, _keys), do: nil
+
+  defp get(data, []), do: data
+
+  defp get(data, keys), do: get_in(data, keys)
 end
