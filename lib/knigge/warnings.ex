f41a2dc4936b8b3a6f7d894b1101e97027dad96c
Marcus Kruse <esurk.sucram@gmail.com>
2022-10-14 17:01:42 +0200

    Run mix format

--- lib/knigge/warnings.ex
+++ lib/knigge/warnings.ex
@@ -27,9 +27,7 @@ defmodule Knigge.Warnings do
 
     There are two ways to resolve this warning:
       1. move the behaviour into a separate module and `use Knigge, behaviour: MyBehaviour`;
-         this enables to compiler to finish compilation of `#{inspect(implementation)}` before compiling `#{
-      inspect(module)
-    }`
+         this enables to compiler to finish compilation of `#{inspect(implementation)}` before compiling `#{inspect(module)}`
       2. pass `delegate_at_runtime?: true` as option, this will move **all** delegation to runtime
     """)
   end
